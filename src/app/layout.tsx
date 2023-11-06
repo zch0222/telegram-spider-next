2fd13bb8720e491862c7df2d0004162feb449c24
YXLM <yxlmzch0222@gmail.com>
2023-11-07 03:04:24 +0800

    feat: 移动端缩放问题

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="zh">
       <head>
+          <meta content="yes" name="apple-mobile-web-app-capable"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
       </head>
       <body className={inter.className}>
