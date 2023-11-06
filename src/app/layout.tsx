336a8c5baafd849189d049617684f99bea6fabf6
YXLM <yxlmzch0222@gmail.com>
2023-11-07 02:56:08 +0800

    feat: 移动端缩放问题

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh">
+      <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+      </head>
       <body className={inter.className}>
         <Providers>
           <AntdRegistry>
